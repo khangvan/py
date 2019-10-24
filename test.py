c06c4daf3e057a34b0d7708ea93a8b84a36e34c2
Khang Van <vanthaikhang@gmail.com>
2019-10-25 01:06:41 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,18 @@ def testrun():
   print("ok")
 def testrun2():
   print("ok2")
+  
+def postgres_passwd(password, username, uppercase=False):
+    """
+    Reference(s):
+        http://pentestmonkey.net/blog/cracking-postgres-hashes/
+    >>> postgres_passwd(password='testpass', username='testuser', uppercase=False)
+    'md599e5ea7a6f7c3269995cba3927fd0093'
+    """
+
+    username = getBytes(username)
+    password = getBytes(password)
+
+    retVal = "md5%s" % md5(password + username).hexdigest()
+
+    return retVal.upper() if uppercase else retVal.lower()
