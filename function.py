6358b822fdcdf3ece4ce4dee3367423ac531e18b
Khang Van <vanthaikhang@gmail.com>
2019-10-28 19:02:39 +0700

    Update function.py

--- function.py
+++ function.py
@@ -9,6 +9,6 @@ def hello():
 def kvan_get_lib(filename):
   import pandas as pd
   link="https://raw.githubusercontent.com/khangvan/py/master/{}".format(filename)
-  !curl -O link
-  import filename
+  
+  
   print("done")
