cb25546928b0b187935b9431b0180cd35bbc2af3
Khang Van <vanthaikhang@gmail.com>
2019-10-28 18:57:06 +0700

    Update function.py

--- function.py
+++ function.py
@@ -6,35 +6,8 @@ def hello():
 
 
 #-----
-def exampleplot():
+def kvan_get_lib(filename)
   import pandas as pd
-  mytext='''
-  2019/01/1,2
-  2019/01/2,4
-  2019/01/3,6
-  2019/01/4,7
-  2019/01/5,3
-  2019/01/5,3
-  '''
-  mycols=['date','qty']
-
-  import sys
-  from io import StringIO
-
-
-  df=pd.read_csv(StringIO(mytext),sep=",")
-  df.columns=mycols
-  y=df.set_index(['date'])
-
-  import matplotlib.pyplot as plt
-  import matplotlib
-  # warnings.filterwarnings("ignore")
-  plt.style.use('fivethirtyeight')
-  matplotlib.rcParams['axes.labelsize'] = 14
-  matplotlib.rcParams['xtick.labelsize'] = 12
-  matplotlib.rcParams['ytick.labelsize'] = 12
-  matplotlib.rcParams['text.color'] = 'G'
-
-  y.plot(figsize=(15, 4))
-  plt.show()
-  print('ok')
+  link="https://raw.githubusercontent.com/khangvan/py/master/{}".format(filename)
+  !curl -O link
+  import filename
